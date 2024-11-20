981230a7336fc24ee87ce79099595d59708f2c59
AmelSAT <aselatt079@g.educaand.es>
2024-11-20 10:01:18 +0100

    Añadimos un método de muestra de números aleatorios

--- src/Tarea3Git/Tarea3.java
+++ src/Tarea3Git/Tarea3.java
@@ -1,10 +1,14 @@
 package Tarea3Git;
-
+//20/11/2024
+import java.util.Random;
 public class Tarea3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		        Random rand = new Random();
+		        
+		        for (int i = 0; i < 20; i++) {
+		            int numeroAleatorio = rand.nextInt(10) + 1; 
+		            System.out.println(numeroAleatorio);
+		        }
 	}
-
 }
